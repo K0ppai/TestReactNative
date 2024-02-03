7727b5317ae96b018a681fa9193e21a98651fda5
koppai <psthu.koppai@gmail.com>
2024-02-03 23:05:40 +0630

    Create datas

---
+++ src/data/data.js
@@ -0,0 +1,62 @@
+export const arrayOfObjects = [
+  {
+    id: 1,
+    title: 'Introduction to JavaScript',
+    description:
+      'Learn the basics of JavaScript programming language and its role in web development.',
+  },
+  {
+    id: 2,
+    title: 'Arrays and Loops',
+    description:
+      'Explore the concepts of arrays and loops in JavaScript for efficient data manipulation and iteration.',
+  },
+  {
+    id: 3,
+    title: 'Object-Oriented Programming',
+    description:
+      'Understand the principles of object-oriented programming (OOP) in JavaScript for building scalable and modular code.',
+  },
+  {
+    id: 4,
+    title: 'Asynchronous JavaScript',
+    description:
+      'Dive into asynchronous programming using Promises and async/await to handle asynchronous operations in JavaScript.',
+  },
+  {
+    id: 5,
+    title: 'DOM Manipulation',
+    description:
+      'Master the Document Object Model (DOM) to dynamically update and manipulate web page content using JavaScript.',
+  },
+  {
+    id: 6,
+    title: 'Responsive Web Design',
+    description:
+      'Explore techniques for creating responsive and mobile-friendly web designs using HTML, CSS, and JavaScript.',
+  },
+  {
+    id: 7,
+    title: 'APIs and Fetch',
+    description:
+      'Learn how to make HTTP requests, interact with APIs, and handle data using the Fetch API in JavaScript.',
+  },
+  {
+    id: 8,
+    title: 'Node.js Basics',
+    description:
+      'Get started with server-side JavaScript using Node.js, and understand its role in building scalable and efficient web applications.',
+  },
+  {
+    id: 9,
+    title: 'React.js Fundamentals',
+    description:
+      'Build interactive user interfaces with React.js, a popular JavaScript library for building modern single-page applications.',
+  },
+  {
+    id: 10,
+    title: 'Express.js for Backend Development',
+    description:
+      'Explore Express.js, a minimalist web application framework for Node.js, to build robust and scalable backend services.',
+  },
+];
